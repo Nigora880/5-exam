734f4570d935670ca55e348c5e9c5c2e87f62856
Nigora880 <nx165880@gmail.com>
2025-07-16 11:36:04 +0300

    update

--- src/modules/Ready.jsx
+++ src/modules/Ready.jsx
@@ -6,7 +6,7 @@ function Ready(){
         <div className="containers flex flex-col ">
             <h2 className="pt-[110px] font-normal text-[40px] leading-[56px] text-[#242F65] w-[344px]">Ready to get start with UIUXer?</h2>
             <p className="w-[513px] font-normal text-[20px] leading-[30px] text-[#525665]">Well-planned layouts with a clear structure that can be utilized to demonstrate your inventiveness in the digital business field.</p>
-            <button className="mt-[18px] w-[274px] h-[69px] bg-[#FF774C] text-[#FFFFFF] text-[18px] leading-[27px] rounded-xl">Get Start Now</button>
+            <button className="mt-[18px] w-[274px] h-[69px] bg-[#FF774C] text-[#FFFFFF] hover:text-[#FF774C] border-[#FF774C] border-[2px] hover:bg-[#FFFFFF] duration-300 text-[18px] leading-[27px] rounded-xl">Get Start Now</button>
         </div>
     </section>
     )
