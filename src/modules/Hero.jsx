c2719cd6e630882660d7eed3ba5ebdaf5dade15b
Nigora880 <nx165880@gmail.com>
2025-07-16 11:33:18 +0300

    update

--- src/modules/Hero.jsx
+++ src/modules/Hero.jsx
@@ -6,8 +6,8 @@ return(
           <h1 className="font-normal text-[100px] leading-[130px] text-[#242F65] text-center mx-auto mt-[50px] " >Tell your creativity to the world with <span className="font-normal text-[100px] leading-[130px] text-[#FF774C]">UIUXer</span></h1>
             <p className='font-normal text-[25px] leading-[37.5px] text-[#242F65] text-center mx-auto w-[500px]'>UIUXer is a creative, minimal, and cleanly designed personal and portfolio Webflow template.</p>
             <div className="flex items-center mx-auto gap-[10px] py-[25px]">
-                <button className='w-[266px] h-[69px] border-[1px] hover:text-[#FF774C]  border-[#FF774C] rounded-sm bg-[#FF774C] text-[#FFFFFF] text-[18px] leading-[27px] font-normal'>Buy Template</button>
-                <button className='w-[266px] h-[69px] border-[1px] hover:text-[#7390F9] border-[#7390F9] rounded-sm bg-[#7390F9] text-[#FFFFFF] text-[18px] leading-[27px] font-normal'>Explore Page</button>
+                <button className='w-[266px] h-[69px] border-[2px] hover:text-[#FF774C] border-[#FF774C] hover:bg-[#FFFFFF] duration-300 rounded-sm bg-[#FF774C] text-[#FFFFFF] text-[18px] leading-[27px] font-normal'>Buy Template</button>
+                <button className='w-[266px] h-[69px] border-[2px] hover:text-[#7390F9] border-[#7390F9] hover:bg-[#FFFFFF] duration-300 rounded-sm bg-[#7390F9] text-[#FFFFFF] text-[18px] leading-[27px] font-normal'>Explore Page</button>
             </div>
             
             <img className='text-center mx-auto py-[41px]' src={Frame} alt="frame" width={203} height={205} />
